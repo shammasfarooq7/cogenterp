56845acd68082360dbc7c62373e4bcfa7e94bf3c
Fur619 <furqanarshad98.fa@gmail.com>
2023-02-01 04:04:39 +0500

    Fix Local Strategy

--- src/users/auth/local.strategy.ts
+++ src/users/auth/local.strategy.ts
@@ -1,18 +1,19 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy } from "passport-local";
 import { AuthService } from "./auth.service";
-import { UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 
-export class LocalStrategy extends PassportStrategy(Strategy){
-  constructor(private authService: AuthService){
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
     super({
       usernameField: 'email'
     })
   }
 
-  async validate(email: string, password: string){
-    const user = await this.authService.validateUser(email, password)
-    if(!user){
+  async validate(username: string, password: string) {
+    const user = await this.authService.validateUser(username, password)
+    if (!user) {
       throw new UnauthorizedException();
     }
     return user;
