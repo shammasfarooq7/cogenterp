e4f1ff8e014f987b9940c17d7560d1b9d68380f2
Shammas <shammasfarooq7@gmail.com>
2023-06-25 00:34:40 +0500

    ticker dependencies

--- src/modules/tickets/tickets.module.ts
+++ src/modules/tickets/tickets.module.ts
@@ -8,12 +8,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TicketAttachment } from './entities/ticketAttachment.entity';
 import { TicketDate } from './entities/ticketDate.entity';
 import { Resource } from '../resources/entity/resource.entity';
+import { CustomerService } from '../customer/customer.service';
+import { Customer } from '../customer/entities/customer.entity';
+import { ProjectService } from '../project/project.service';
+import { Jobsite } from '../jobsite/entities/jobsite.entity';
+import { JobsiteService } from '../jobsite/jobsite.service';
+import { Project } from '../project/entities/project.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Role } from 'src/users/entities/role.entity';
+import { RoleService } from 'src/users/role.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Ticket, TicketDate, TicketDetail, TimeSheet, TicketAttachment, Resource])
+    TypeOrmModule.forFeature([Ticket, TicketDate, TicketDetail, TimeSheet, TicketAttachment, Resource, Customer, Jobsite, Project, User, Role])
   ],
-  providers: [TicketsResolver, TicketsService, TypeOrmModule, TicketDate],
+  providers: [TicketsResolver, TicketsService, TypeOrmModule, TicketDate, CustomerService, ProjectService, JobsiteService, RoleService],
   exports: [TicketsService]
 
 })
